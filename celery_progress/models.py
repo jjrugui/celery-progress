ec9b7e0775fdf11a2f3107bc4ffb9c0388970109
juanjo <jurugu@mailbox.org>
2020-12-17 16:41:23 +0100

    feat(models): add task state to the model
    Model field to be able to easily filter task states

--- celery_progress/models.py
+++ celery_progress/models.py
@@ -2,10 +2,17 @@ import uuid
 from django.db import models
 from django.conf import settings
 
+class STATES(models.TextChoices):
+    STARTED = "STARTED"
+    FINISHED = "FINISHED"
+    FAILED = "FAILED"
+    PENDING = "PENDING"
+
 class ProgressPersistent(models.Model):
     task_id = models.UUIDField(default=uuid.uuid4)
     completed = models.BooleanField(default=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    state =  models.CharField(max_length=15, choices=STATES.choices, default=STATES.PENDING)
 
     
 
